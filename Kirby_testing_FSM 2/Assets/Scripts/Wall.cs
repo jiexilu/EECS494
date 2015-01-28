ffc7719067d595ab20ee983b55e2cf567f517df7
Anna Do <doannado@gmail.com>
2015-01-28 14:45:17 -0500

    added the music bubble, more enemies and fixed beam

---
+++ Kirby_testing_FSM 2/Assets/Scripts/Wall.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : MonoBehaviour {
+
+	private float pos1;
+	// Use this for initialization
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col){
+		if (tag == "Wall") {
+			if (col.gameObject.tag != "Ground" && col.gameObject.tag != "Ceiling") {
+					pos1 = col.gameObject.transform.position.x;		
+			}
+		}
+		else if (tag == "Ceiling" && col.gameObject.tag != "Wall") {
+			pos1 = col.gameObject.transform.position.y;	
+			//print ("ceiling collid");
+		}
+	}
+
+	void OnTriggerStay(Collider col){
+		Vector3 player = col.gameObject.transform.position;
+		if (tag == "Wall" && col.gameObject.tag != "Ground") {
+			//print ("Wall collide");
+			player.x = pos1;
+			col.gameObject.transform.position = player;
+		}
+		else if (tag == "Ceiling" && col.gameObject.tag != "Wall") {
+			player.y = pos1;
+			col.gameObject.transform.position = player;
+		}
+	}
+}
---
+++ Kirby_testing_FSM 2/Assets/Scripts/music.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class music : MonoBehaviour {
+
+	public Kirby kirby;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if (col.tag == "Player") {
+			kirby.power = power_type.sing;
+			kirby.cur_state = State.stand_power;
+			gameObject.SetActive(false);
+		}
+	}
+}
