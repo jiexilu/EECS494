94504505b3db57b0338c4db44ded5c4bf7f00cca
Cici Lu <jiexi.cici.lu@gmail.com>
2015-02-04 11:07:10 -0500

    moved files

---
+++ Kirby_testing_FSM/Assets/Scripts/Spawner.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject my_enemy;
+	private Vector3 position = new Vector3 ();
+
+	// Use this for initialization
+	void Start () {
+		position = gameObject.transform.position;
+		position.z = 7.16f;
+		gameObject.transform.position = position;
+//		my_enemy.gameObject.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	//initial view
+	void OnBecameVisible(){
+//		my_enemy.gameObject.SetActive (true);
+//		position.z = 0;
+//		my_enemy.transform.position = position;
+	}
+
+	void OnBecameInvisible(){
+//		can only test this when away from the scene view in unity
+		if (my_enemy == null) return; 
+		print ("spawner out of sight");
+		if (my_enemy.activeSelf == false) {
+			print ("enemy spawn");
+			my_enemy.SetActive (true);
+			position.z = 0;
+			my_enemy.transform.position = position;
+		}
+		if(my_enemy.transform.position.y < -35.46){
+			my_enemy.SetActive(false);
+		}
+	}
+
+}
---
+++ Kirby_testing_FSM/Assets/Scripts/main_menu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class main_menu : MonoBehaviour {
+		
+	public GameObject subMenu;
+
+	// Use this for initialization
+	void Start () {
+		subMenu.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return)) {
+			subMenu.SetActive(true);
+		}
+	}
+}
